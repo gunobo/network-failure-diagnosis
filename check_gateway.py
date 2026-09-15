aac638ad513743b19780c3e1a0d92b753df45032
beomhobag7-byte <beomhobag7@gmail.com>
2026-09-15 12:20:40 +0900

    게이트웨이, 퍼포먼스 작성 끝

--- check_gateway.py
+++ check_gateway.py
@@ -1,6 +1,6 @@
 def check_gateway(facts):
     if facts.get("IP정상") is True and facts.get("Gateway핑성공") is False:
         return "R8"
-    if facts.get("Gateway외부핑성공") is True and facts.get("외부IP핑성공") is False:
+    if facts.get("Gateway핑성공") is True and facts.get("외부IP핑성공") is False:
         return "R9"
     return None
--- check_performance.py
+++ check_performance.py
@@ -1,5 +1,8 @@
-if facts.get("연결표시") == "없음":
-        return "R1"
-    if facts.get("특정PC만안됨"):
-        return "R2"
+def check_perfomance(facts):
+    if facts.get("연결성공") is True and fact.get("응답느림") is True and fact.get("RTT높음"):
+        return "R15"
+    if facts.get("패킷손실률높음") is True:
+        return "R16"
+    if facts.get("웹느림") is True and facts.get("모바일데이터정상") is True:
+        return "R17"
     return None
